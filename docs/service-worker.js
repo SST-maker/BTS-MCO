8072f83edc9a57f6797a005b335042271c2cd249
SST-maker <nacerito83@gmail.com>
2026-08-26 18:26:21 +0200

    Add files via upload

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='mco-quiz-v12-header-marquee';
-const RUNTIME='mco-quiz-v12-runtime';
+const CACHE='mco-quiz-v12-1-signature';
+const RUNTIME='mco-quiz-v12-1-runtime';
 const ASSETS=[
   './','./index.html','./offline.html','./teacher.html','./teacher.js','./students.html','./students.js','./progression.html','./progression.js',
   './pedagogy.html','./pedagogy.js','./revisions.html','./revisions.js','./student-revisions.html','./student-revisions.js',
